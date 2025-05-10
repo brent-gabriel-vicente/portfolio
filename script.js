e2edcab90189371614e7d1085e5ea760715317c7
Brent-Gabriel-Vicente <vicentebrentgabriel@gmail.com>
2025-05-10 12:07:33 -0800

    About page

--- script.js
+++ script.js
@@ -26,41 +26,41 @@ const words = ["a Student", "an IT", "a Developer"];
       } else {
         setTimeout(typeEffect, isDeleting ? 100 : 200);
       }
-    }
+}
 
-    function showSection(sectionId) {
-      const sections = document.querySelectorAll('section');
-      sections.forEach(section => section.style.display = 'none');
+function showSection(sectionId) {
+    const sections = document.querySelectorAll('section');
+    sections.forEach(section => section.style.display = 'none');
 
-      const selectedSection = document.getElementById(sectionId);
-      selectedSection.style.display = 'block';
+    const selectedSection = document.getElementById(sectionId);
+    selectedSection.style.display = 'block';
 
-      const navLinks = document.querySelectorAll('nav a');
-      navLinks.forEach(link => link.classList.remove('active'));
+    const navLinks = document.querySelectorAll('nav a');
+    navLinks.forEach(link => link.classList.remove('active'));
 
-      const activeLink = document.querySelector(`nav a[href="#${sectionId}"]`);
-      if (activeLink) activeLink.classList.add('active');
-    }
+    const activeLink = document.querySelector(`nav a[href="#${sectionId}"]`);
+    if (activeLink) activeLink.classList.add('active');
+}
 
-    document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
       typeEffect();
       showSection('home');
 
-      VANTA.NET({
-        el: "#vanta-bg",
-        backgroundAlpha: 1,
-        backgroundColor: 0x0,
-        color: 0xff,
-        maxDistance: 19,
-        points: 10,
-        spacing: 15,
-        showDots: true,
-        mouseControls: true,
-        touchControls: false,
-        gyroControls: false,
-        minHeight: 200,
-        minWidth: 200,
-        scale: 1,
-        scaleMobile: 1
-      });
+  VANTA.NET({
+      el: "#vanta-bg",
+      backgroundAlpha: 1,
+      backgroundColor: 0x0,
+      color: 0xff,
+      maxDistance: 19,
+      points: 10,
+      spacing: 15,
+      showDots: true,
+      mouseControls: true,
+      touchControls: false,
+      gyroControls: false,
+      minHeight: 200,
+      minWidth: 200,
+      scale: 1,
+      scaleMobile: 1
     });
+});
